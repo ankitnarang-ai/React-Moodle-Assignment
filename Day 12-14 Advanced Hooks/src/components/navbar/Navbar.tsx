a7c2c2c760a64023dd3e4678441caa8f87b76e1b
ankitnarang <ankit2@relinns.com>
2024-07-13 19:31:31 +0530

    - [Added] Filter : In Progress

--- Day 12-14 Advanced Hooks/src/components/navbar/Navbar.tsx
+++ Day 12-14 Advanced Hooks/src/components/navbar/Navbar.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useSelector } from 'react-redux';
+import { useDispatch } from "react-redux";
+import { addItems } from "../../reducer/itemSlice";
 const Navbar = () => {
   const userData = localStorage.getItem('user');
-
+  const [searchQuery, setSearchQuery] = useState('')
+  const selector: any =  useSelector((state: any) => state.item.items);
+  const dispatch = useDispatch();
+  const handleSearchChange =(e: any)=>{
+    setSearchQuery(e.target.value);
+    const selectedItem = selector.filter((item: any) =>
+      item.title.toLowerCase().startsWith(e.target.value.toLowerCase())
+    );
+    if(e.target.value === ''){
+      dispatch(addItems(selector))
+    }
+    dispatch(addItems(selectedItem))
+    console.log("Filtered items", selectedItem);
+  }
   return (
     <div className="navbar-container">
       <div className="navbar-logo">
@@ -11,7 +27,11 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="navbar-search">
-        <input type="text" placeholder="Search" />
+        <input 
+        type="text" 
+        placeholder="Search"
+        value={searchQuery}
+        onChange={handleSearchChange} />
       </div>
       <div className="navbar-buttons">
         <Link to="/cart">
